fd0f2500329f9cd8e25e78119c3022370caaa34b
Anand3891 <anand.imac.sri3@outlook.com>
2020-01-17 22:09:28 +0000

    sd

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function myFunction() {
   var x = document.getElementById("myInput");
   if (x.type === "password") {
@@ -13,7 +12,7 @@ function validate() {
   var pw = document.myform.pword.value;
   var valid = false;
   var unArray = ["Archit", "Sreehitha", "Nakul", "Vrinda", "Diya", "Keshav", "Yuvraj", "Lucas", "Eric", "Maanika", "Dhruva", "Anuj", "Nyan", "Roonhi", "Saachi", "Pranesh", "Anand", "Nathan", "Layton", "Kathy", "admin"];
-  var pwArray = ["6COverlook", "6COverlook", "debatechiuoaamp246", "6COverlook", "6COverlook", "6COverlook", "6COverlook", "lucaca", "badatbrawlstars", "6COverlook", "6COverlook", "football123", "poopyface", "6COverlook", "6COverlook", "pranpran#1", "1141144", "Jeffy", "SAO", "6COverlook", "admin"];
+  var pwArray = [/*Archit*/ "archeetos", /*Sreehitha*/ "6COverlook","debatechamp246", "6COverlook", "6COverlook", , "anything", /*Lucas*/"Lucass", /*Eric*/"Menggg", /*Maanika*/"6COverlook", /*Dhruva*/"6COverlook", /*Anuj*/"football123", /*Nyan*/"poopyface", "SRS", "tramarat", "justkeepitnormal", "1141144", "nasangswee", "fairytail", "6COverlook", "admin"];
   for (var i = 0; i < unArray.length; i++) {
     if ((un == unArray[i]) && (pw == pwArray[i])) {
       valid = true;
@@ -38,7 +37,6 @@ function validate() {
     count--;
   }
   else {
-
     return true;
   }
 }
